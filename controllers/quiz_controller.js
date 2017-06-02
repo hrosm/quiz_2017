7b3f46e81c1a67e7929a65b5ac25bb2fbc98c901
hrosm <hector.rosm@gmail.com>
2017-06-02 19:12:40 +0200

    Juego aleatorio

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -192,7 +192,7 @@ exports.check = function (req, res, next) {
 exports.randomplay = function(req, res, next) {
 
     var array = models.Quiz.findAll();
-    var aleatorio = Math.random()*array.length;
+    var aleatorio = Math.random()*array.length();
     //var newId = models.Quiz.findById(aleatorio);
 models.Quiz.findById(aleatorio)
 .then(function (quiz){
