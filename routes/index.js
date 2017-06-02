868567880ce83a934273208f3e8ccd90a134fbf2
hrosm <hector.rosm@gmail.com>
2017-06-02 18:15:20 +0200

    Juego aleatorio

--- routes/index.js
+++ routes/index.js
@@ -33,5 +33,8 @@ router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
+router.get('/quizzes/randomplay',		   quizController.play);
+router.get('/quizzes/randomcheck/:quizId?answer=respuesta',  quizController.check);
+
 
 module.exports = router;
