6037c4cf7f9c2307bb1544fd48498bba1af810ca
hrosm <hector.rosm@gmail.com>
2017-06-05 17:24:29 +0200

    Juego aleatorio

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -215,7 +215,7 @@ exports.randomcheck = function(req, res, next){
     var answer = req.query.answer || "";
 
     var result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
-    var score = ++req.query.score++ || 0;
+    var score = ++req.query.score || 0;
     res.render('quizzes/random_result', {
         score: score,
         answer: answer, 
